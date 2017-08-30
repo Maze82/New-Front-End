51bf8fe8d6665f62d6db589303e63d6466c570e6
Maze82 <maze@hotmail.com.au>
2017-08-30 11:50:43 +1000

    reports php page

---
+++ reports.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>reports</title>
+  </head>
+
+
+  <body>
+    <input type:"date" name="datefrom" value="">
+    <input type:"date" name="dateto" value="">
+  </body>
+</html>
+
+
+
+
+
+
+
+<?php
+
+
+
+
+ ?>
